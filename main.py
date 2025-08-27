a01f33bb4cbe12e370534a77a8e8548824bd723f
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2025-08-27 12:53:15 -0700

    add each stattus labels

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class PipewireGUI:
         self.root.geometry(WINDOW_GEOMETRY)
         self.root.resizable(False, False)
         self.root.title(WINDOW_TITLE)
-        self.root.configure(bg=COLORS["bg_primary"], padx=10, pady=10)
+        self.root.configure(bg=COLORS["bg_primary"], padx=5, pady=5)
 
     def _setup_ttk_style(self) -> None:
         self.style = Style()
@@ -126,29 +126,38 @@ class PipewireGUI:
     def _create_current_status_section(self) -> None:
         frame = Frame(self.root, style="Main.TFrame")
         frame.pack(pady=(10, 10))
-        for i, weight in enumerate([1, 0, 0, 1]):
+        for i, weight in enumerate([1, 0, 0, 0, 0, 1]):
             frame.grid_columnconfigure(i, weight=weight)
+        for i, weight in enumerate([0, 1]):
+            frame.grid_rowconfigure(i, weight=weight)
 
+        self._create_status_title_label(frame, SAMPLE_RATE_CONFIG["title"], 0, 0)
         self._current_sample_rate_label = self._create_status_value_label(
-            frame, Formatters.format_sample_rate(None), 0, width=5
+            frame, Formatters.format_sample_rate(None), 1, 0, width=5
         )
-        self._create_unit_label(frame, SAMPLE_RATE_CONFIG["unit"], 1)
-        self._current_buffer_size_label = self._create_status_value_label(
-            frame, Formatters.format_buffer_size(None), 2, width=4
-        )
-        self._create_unit_label(frame, QUANTUM_CONFIG["unit"], 3)
-        self._current_latency_label = self._create_status_value_label(
-            frame, Formatters.format_latency(None), 5, width=4
-        )
-        self._create_unit_label(frame, LATENCY_CONFIG["unit"], 6)
+        self._create_unit_label(frame, SAMPLE_RATE_CONFIG["unit"], 1, 1)
 
-    def _create_status_title_label(self, parent: Frame, text: str, column: int) -> None:
+        self._create_status_title_label(frame, QUANTUM_CONFIG["title"], 0, 2)
+        self._current_buffer_size_label = self._create_status_value_label(
+            frame, Formatters.format_buffer_size(None), 1, 2, width=4
+        )
+        self._create_unit_label(frame, QUANTUM_CONFIG["unit"], 1, 3)
+
+        self._create_status_title_label(frame, LATENCY_CONFIG["title"], 0, 5)
+        self._current_latency_label = self._create_status_value_label(
+            frame, Formatters.format_latency(None), 1, 5, width=4
+        )
+        self._create_unit_label(frame, LATENCY_CONFIG["unit"], 1, 6)
+
+    def _create_status_title_label(
+        self, parent: Frame, text: str, row: int, column: int
+    ) -> None:
         Label(parent, text=text, style="Status.Title.TLabel").grid(
-            row=0, column=column, padx=0, pady=0, sticky="sw"
+            row=row, column=column, padx=0, pady=0
         )
 
     def _create_status_value_label(
-        self, parent: Frame, text: str, column: int, width: int
+        self, parent: Frame, text: str, row: int, column: int, width: int
     ) -> Label:
         label = Label(
             parent,
@@ -158,19 +167,22 @@ class PipewireGUI:
             width=width,
             padding=(10, 2, 10, 2),
         )
-        label.grid(row=0, column=column, padx=0, pady=0, sticky="e")
+        label.grid(row=row, column=column, padx=0, pady=0, sticky="e")
         return label
 
-    def _create_unit_label(self, parent: Frame, text: str, column: int) -> None:
-        padding = (0, 0, 10, 0) if column == 1 else (0, 0, 0, 0)
-        Label(parent, text=text, style="Status.Unit.TLabel", padding=padding).grid(
-            row=0, column=column, padx=5, pady=0, sticky="sw"
-        )
+    def _create_unit_label(
+        self, parent: Frame, text: str, row: int, column: int
+    ) -> None:
+        Label(
+            parent,
+            text=text,
+            style="Status.Unit.TLabel",
+        ).grid(row=row, column=column, padx=(2, 8), pady=0, sticky="sw")
 
     def _create_buttons_section(self, config: ConfigDict, buttons: ButtonDict) -> None:
         Label(
             self.root, text=config["title"], style="Title.TLabel", anchor="center"
-        ).pack(pady=(10, 0))
+        ).pack(pady=(5, 0))
         self._create_buttons(config, buttons)
 
     def _create_buttons(self, config: ConfigDict, buttons: ButtonDict) -> None:
